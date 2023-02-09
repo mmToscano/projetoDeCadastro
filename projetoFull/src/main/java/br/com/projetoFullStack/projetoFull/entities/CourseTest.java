6106afcf0375a6be71e2890e9d9d2988b53b7e38
mmToscano <107902881+mmToscano@users.noreply.github.com>
2023-02-09 15:46:13 -0300

    Add files via upload

--- projetoFull/src/main/java/br/com/projetoFullStack/projetoFull/entities/CourseTest.java
+++ projetoFull/src/main/java/br/com/projetoFullStack/projetoFull/entities/CourseTest.java
@@ -1,6 +1,5 @@
 package br.com.projetoFullStack.projetoFull.entities;
 
-import br.com.projetoFullStack.projetoFull.entities.classesToBeInherited.Test;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,16 +11,21 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "coursetest")
-public class CourseTest extends Test{
+public class CourseTest{
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer idCourseTest;
 	
+	@Column(name = "course_test_name", nullable=false)
+	private String testName;
 	
+	@Column(name = "course_test_grade", nullable=false)
+	private Double fullGrade;
 	
 	@Column(name = "course_test_creation_day", nullable=false)
 	private String courseTestCreationDay;
+	
 	
 	@ManyToOne
 	@JoinColumn(name="course_test_course_id")
@@ -29,44 +33,84 @@ public class CourseTest extends Test{
 
 	public CourseTest() {}
 	
-	public CourseTest(String courseTestName, Double courseTestFullGrade, Integer idCourseTest, String courseTestCreationDay,
-			Integer courseTestGrade) {
+	
+
+	public CourseTest(Integer idCourseTest, String testName, Double fullGrade, String courseTestCreationDay,
+			Course course) {
 		super();
-		this.setFullGrade(courseTestFullGrade);
-		this.setName(courseTestName);
 		this.idCourseTest = idCourseTest;
+		this.testName = testName;
+		this.fullGrade = fullGrade;
 		this.courseTestCreationDay = courseTestCreationDay;
+		this.course = course;
 	}
+
+
 
 	public Integer getIdCourseTest() {
 		return idCourseTest;
 	}
 
+
+
 	public void setIdCourseTest(Integer idCourseTest) {
 		this.idCourseTest = idCourseTest;
 	}
 
+
+
+	public String getTestName() {
+		return testName;
+	}
+
+
+
+	public void setTestName(String testName) {
+		this.testName = testName;
+	}
+
+
+
+	public Double getFullGrade() {
+		return fullGrade;
+	}
+
+
+
+	public void setFullGrade(Double fullGrade) {
+		this.fullGrade = fullGrade;
+	}
+
+
+
 	public String getCourseTestCreationDay() {
 		return courseTestCreationDay;
 	}
+
+
 
 	public void setCourseTestCreationDay(String courseTestCreationDay) {
 		this.courseTestCreationDay = courseTestCreationDay;
 	}
 
 
+
 	public Course getCourse() {
 		return course;
 	}
+
+
 
 	public void setCourse(Course course) {
 		this.course = course;
 	}
 
+
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
-		int result = super.hashCode();
+		int result = 1;
 		result = prime * result + ((idCourseTest == null) ? 0 : idCourseTest.hashCode());
 		return result;
 	}
@@ -75,7 +119,7 @@ public class CourseTest extends Test{
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (!super.equals(obj))
+		if (obj == null)
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
@@ -87,6 +131,12 @@ public class CourseTest extends Test{
 			return false;
 		return true;
 	}
+
+	
+
+	
+
+	
 	
 	
 	
